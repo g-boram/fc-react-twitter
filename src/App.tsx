340b789326b28665d0bb0da8954e0237e2c9003c
GOBORAM <109337202+g-boram@users.noreply.github.com>
2024-01-06 16:42:01 +0900

    scss 스타일링 (1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import './App.css';
+import { Layout } from 'components/Layout';
 import Router from 'components/Router';
 
 
 function App() {
   return (
-    <Router />
+    <Layout>
+      <Router />
+    </Layout>
   );
 }
 
