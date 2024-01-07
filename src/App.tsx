9a7212b51e996188f56e00ebc89be821e1c9f175
GOBORAM <109337202+g-boram@users.noreply.github.com>
2024-01-07 17:49:02 +0900

    firebase 세팅하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-import { Layout } from 'components/Layout';
 import Router from 'components/Router';
+import { useState }from 'react';
+import { Layout } from 'components/Layout';
+import { getAuth } from "firebase/auth";
+import { app } from "firebaseApp";
 
 
 function App() {
+  const auth = getAuth(app);
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
+    !!auth?.currentUser
+  );
+
+  console.log("auth -----> ", auth);
+  console.log("isAuthenticated -----> ", isAuthenticated);
+
+
   return (
     <Layout>
       <Router />
