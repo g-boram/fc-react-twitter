b6fca6833b0184af38bb2db1adfb766325f9e5b7
GOBORAM <109337202+g-boram@users.noreply.github.com>
2024-01-08 14:27:43 +0900

    firebase-인증구현
    (1) 기본/ 깃허브/ 구글 로그인
    (2) 기본/ 깃허브/ 구글 회원가입
    (3) 로그아웃

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,40 @@
-import Router from 'components/Router';
-import { useState }from 'react';
-import { Layout } from 'components/Layout';
-import { getAuth } from "firebase/auth";
+import Router from "components/Router";
+import Loader from "components/loader/Loader";
+import { Layout } from "components/Layout";
+import { useState, useEffect } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { app } from "firebaseApp";
+import { ToastContainer } from "react-toastify";
 
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   const auth = getAuth(app);
+  const [init, setInit] = useState<boolean>(false);
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
     !!auth?.currentUser
   );
 
-  console.log("auth -----> ", auth);
-  console.log("isAuthenticated -----> ", isAuthenticated);
-
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsAuthenticated(true);
+      } else {
+        setIsAuthenticated(false);
+      }
+      setInit(true);
+    });
+  }, [auth]);
 
   return (
     <Layout>
-      <Router />
+      <ToastContainer 
+        theme="dark" 
+        autoClose={1000} 
+        hideProgressBar
+        newestOnTop
+      />
+      {init ? <Router isAuthenticated={isAuthenticated} /> : <Loader />}
     </Layout>
   );
 }
---
+++ src/components/loader/Loader.tsx
@@ -0,0 +1,8 @@
+import style from "./Loader.module.scss";
+
+export default function Loader() {
+
+  return (
+    <div className={style.loader}>Loader</div>
+  )
+}
